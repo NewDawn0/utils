de31d4e1a239d119fc89b1f19c6917e331a6369d
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-11-30 23:04:30 +0100

    updated readme && added serde-ffmpeg

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # utils
-Some utilites
+Some useful commandline utilities written by me
+
+## Features
+- **serde-ffmpeg** FFMPEG using parallel conversersion
+
+## Changelog
+- 30.11.2020 Added serde-ffmpeg
---
+++ src/serde-ffmpeg/serde-ffmpeg.c
@@ -0,0 +1,13 @@
+// serde ffmpeg
+// Parralelized ffmpeg action
+// Written by NewDawn0 (Tom) 30.11.2022
+
+// Libs
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Main
+int main() {
+}
