6bf035b98eeffa28a856a679168e3f2ea571aa19
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-12-04 06:30:11 +0100

    added a README to vocab

--- src/ffc/ffc.c
+++ src/ffc/ffc.c
@@ -3,11 +3,14 @@
 // Written by NewDawn0 (Tom) 1.12.2022
 
 // Libs
+#include <ftw.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
 // Static Stuff 
 #define HELP "ffc - A threade wrapper aroud the ffmpeg file conversions\nUSAGE: ffc -i <file> -o <outfile>"
@@ -17,14 +20,18 @@ char *cliArgs[2] = {"-i", "-o"};
 void splitVideos(char *inFile);
 int in(char **arr, int len, char *target);
 bool fileExists(char *fname);
+int rmrf(char *path);
+char getExt(char *inFile);
 
 // Main
 int main(int argc, char *argv[]) {
-    // Parse arguments
+    //// Parse arguments ////
+    // check if no arguments were provided
     if (argc == 1) {
         printf("%s\n", HELP);
         return 1;
     }
+    // check all arguments
     int inFileIndex = -1;
     int outFileIndex = -1;
     bool inFileFound = false;
@@ -49,11 +56,13 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+    // check if input and output files were provided
     if (inFileIndex == -1 && outFileIndex == -1) {
         printf("in: %d; out: %d", inFileIndex, outFileIndex);
         fprintf(stderr, "No input and output files provided");
         return 1;
     }
+    // check if file exist
     if (!fileExists(argv[inFileIndex])) {
         fprintf(stderr, "ERROR: Input file \'%s\' does not exist\n", argv[inFileIndex]);
         return 1;
@@ -61,29 +70,38 @@ int main(int argc, char *argv[]) {
     if (fileExists(argv[outFileIndex])) {
         fprintf(stderr, "ERROR: Output file \'%s\' already exists\n", argv[outFileIndex]);
         return 1;
-    }
-    // Check if files exist
-    /* for (int i = 0; i < inFileVec.count; i++) { */
-    /*     char *current = (char*)inFileVec.data[i]; */
-    /*     if (!fileExists(current)) { */
-    /*         fprintf(stderr, "ERROR: \'%s\' No such file or directory\n", current); */
-    /*         return 1; */
-    /*     } */
-    /* } */
-    char inFile[] = "Hello world";
-    splitVideos(inFile);
+    } 
+    splitVideos(argv[inFileIndex]);
     return 0;
 }
 
-// Split Videos into multiple files
+// Split file into multiple files
 void splitVideos(char *inFile) {
+    // set strings
+    char *dir;
     char *command;
-    asprintf(&command, "Lol %s", inFile);
-    printf("%s\n", command);
+    char ext[strlen(getExt(inFile)) + 1];
+    strcpy(ext, getExt(inFile));
+        /* char ext[strlen(strrchr(inFile, '.') + 1) + 1]; */
+        /* strcpy(ext, strrchr(inFile, '.') + 1); */
+    asprintf(&dir, ".%s.tmp-ffc", inFile);
+    asprintf(&command, "ffmpeg -i %s -ss 164 -f segment -segment_time 120 -reset_timestamps -map 0:0 -an %s out%d.%s", inFile, ext);
+    printf("%s", command);
+    // create temporary dir
+    if (mkdir(dir, 0700) == -1) {
+        fprintf(stderr, "ERROR: Failed to create temporary directory \'%s\'\n", dir);
+        exit(1);
+    }
+    if (rmrf(dir) != 0) {
+        fprintf(stderr, "ERROR: Failed to remove temporary directory \'%s\'\n", dir);
+        exit(1);
+    }
+    // remove temporary dir
     free(command);
+    free(dir);
 }
 
-// Retrurn indes of allowed arguments
+// Return indes of allowed arguments
 int in(char **arr, int len, char *target) {
     int i;
     for (i = 0; i < len; i++) {
@@ -100,6 +118,30 @@ bool fileExists(char *fname) {
         return true;
     }
     return false;
+}
+// remove files
+int _unlink(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+    int rv = remove(fpath);
+    if (rv) {
+        perror(fpath);
+    }
+    return rv;
+};
+int rmrf(char *path) {
+    return nftw(path, _unlink, 64, FTW_DEPTH | FTW_PHYS);
+}
+// get file extension
+char getExt(char *inFile) {
+    if (!strchr(inFile, '.')) {
+        perror("Could not get extension");
+        return false;
+    } else {
+        char ext[strlen(strrchr(inFile, '.') + 1) + 1];
+        strcpy(ext, strrchr(inFile, '.') + 1);
+        return ext;
+    }
+    char iex[] = "";
+    return iex;
 }
 // Command
 // fmpeg -i "input" -ss 164 -f segment -segment_time 120 -reset_timestamps 1 -map 0:0 -an output_video%d.MTS
