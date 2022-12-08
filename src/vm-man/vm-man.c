76ceb72e653d6217ec2c3590660f37d9d68deadd
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-12-08 23:17:29 +0100

    readded argparser as a submodule to common

--- src/vm-man/vm-man.c
+++ src/vm-man/vm-man.c
@@ -1,5 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+char HELP[] = "vm-man - A simple QEMU vm managing tool written in c\nUsage:\n\tvm-man install <iso image>\n\tvm-man help\n\tvm-man list\n\tvm-man <list item>";
+char *opts[4] = {"start", "install", "help", "list"};
+enum Mode{Start, Install, Help, List};
+enum Mode mode = 2;
+int defSize = 20;
+int defRam = 4096;
 
-int main(int argc, char *argv[], char **envp) {
+// function init
+int in(char **arr, int len, char *target);
+void doStuff();
+// main
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        fprintf(stderr, "%s\n", HELP);
+        exit(1);
+    } else {
+        int inIndex = in(opts, 4, argv[1]);
+        if (inIndex == -1) {
+            fprintf(stderr, "Error invalid argument\nRun 'vm-man help' for more info");
+            exit(1);
+        } else {
+            mode = inIndex;
+        }
+    }
+    doStuff(argc, argv);
     return 0;
+}
+void doStuff(int argc, char *argv[]) {
+    char *command = NULL;
+    if (mode == Help) {
+        printf("%s\n", HELP);
+    } else if (mode == Install) {
+        asprintf(&command, "qemu-img create -f qcow2 %s %dG", command, defSize);
+        system("echo Heloo");
+        command = NULL;
+    }
+    free(command); 
+}
+int in(char **arr, int len, char *target) {
+    int i;
+    for (i = 0; i < len; i++) {
+        if (strncmp(arr[i], target, strlen(target)) == 0) {
+            return i;
+        }
+    }
+    return -1;
 }
