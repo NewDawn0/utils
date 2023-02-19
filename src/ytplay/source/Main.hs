4523c867880fca7cba36c550546452cc6561c676
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-02-19 16:50:24 +0100

    aded network setup

--- src/ytplay/source/Main.hs
+++ src/ytplay/source/Main.hs
@@ -4,10 +4,16 @@ module Main where
 -- Imports
 import System.Environment (getArgs)
 import Data.List (intercalate)
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 
 -- Fn main
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  let url = "https://vid.puffyan.us/serach?q=" ++ concatArgs args
+  putStrLn url
 
 -- Fn concatArgs: Concats an array of string with '+' as the seperator
 -- @PARAM: [String]
@@ -15,3 +21,7 @@ main = putStrLn "Hello, Haskell!"
 concatArgs :: [String] -> String
 concatArgs = intercalate "+"
 
+-- Fn request: Makes a GET request to the url
+-- @PARAM: String
+-- @RVAL: IO String
+-- requestUrlContent :: String -> IO String
